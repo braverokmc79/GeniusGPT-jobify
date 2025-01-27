6bc96e0216cd3f99cce4298bfb203b4390002bbe
braverokmc79 <braverokmc79@gmail.com>
2025-01-27 20:11:49 +0900

    2. shadcn의 Dropdown Menu 컴포넌트 설치 사용법

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,64 @@
-import { Button } from "@/components/ui/button";
+"use client"
+ 
+import * as React from "react"
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+ 
+type Checked = DropdownMenuCheckboxItemProps["checked"]
+
+
 import { Camera } from "lucide-react";
 
 export default function Home() {
+  const [showStatusBar, setShowStatusBar] = React.useState<Checked>(true)
+  const [showActivityBar, setShowActivityBar] = React.useState<Checked>(false)
+  const [showPanel, setShowPanel] = React.useState<Checked>(false)
+
   return (
     <div className="flex h-screen justify-center  items-center">
       <Button> default Button</Button>
       <Button variant="outline" size="icon" >
         <Camera />
       </Button>
+
+
+      <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">Open</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuCheckboxItem
+          checked={showStatusBar}
+          onCheckedChange={setShowStatusBar}
+        >
+          Status Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={showActivityBar}
+          onCheckedChange={setShowActivityBar}
+          disabled
+        >
+          Activity Bar
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={showPanel}
+          onCheckedChange={setShowPanel}
+        >
+          Panel
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
     </div>
   );
 }
