d7a185b785b49afd89cfdfdf95cbc62d55ab5a46
braverokmc79 <braverokmc79@gmail.com>
2025-01-31 20:03:51 +0900

    17.React Query - 적용하기

--- src/app/(dashboard)/add-job/page.tsx
+++ src/app/(dashboard)/add-job/page.tsx
@@ -1,18 +1,17 @@
 import CreateJobFrom from '@/components/CreateJobFrom';
-//import { HydrationBoundary , dehydrate, QueryClient } from '@tanstack/react-query';
+import { HydrationBoundary , dehydrate, QueryClient } from '@tanstack/react-query';
 import React from 'react'
 
 
 const AddJobPage:React.FC = () => {
 
-  //const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
   return (
-    <>
-      <CreateJobFrom />   
-    {/* <HydrationBoundary state={dehydrate(queryClient)}>
+    <>      
+     <HydrationBoundary state={dehydrate(queryClient)}>
       <CreateJobFrom />      
-    </HydrationBoundary> */}
+    </HydrationBoundary> 
     </>
   )
 }
--- src/components/CreateJobFrom.tsx
+++ src/components/CreateJobFrom.tsx
@@ -3,14 +3,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "./ui/button";
 import { Form } from "./ui/form";
-//import {Input} from '@/components/ui/input';
 import { createAndEditJobSchema, CreateAndEditJobType, JobMode, JobStatus } from "@/dto/JobDTO";
 import CustomFormSelect, { CustomFormField } from "./FormComponents";
-
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useToast } from "@/hooks/use-toast";
+import { createJobAction } from "@/actions/add-job/jobActions";
+import { useRouter } from 'next/navigation';
 
 
 function CreateJobForm() {
-
+  
   const form =useForm<CreateAndEditJobType>({
       resolver: zodResolver(createAndEditJobSchema), 
       defaultValues: {
@@ -22,8 +24,31 @@ function CreateJobForm() {
       }
   });
 
+  const queryClient=  useQueryClient();
+  const {toast}  =useToast();
+  const router = useRouter();
+  const {mutate, isPending} = useMutation({
+    mutationFn: (values:CreateAndEditJobType) => createJobAction(values),
+    onSuccess: (data) => {
+      if(!data){
+        toast({description:"에러가 발생 했습니다.", variant:"destructive"}); 
+        return;       
+      }
+
+      toast({description:"직업가 추가 되었습니다."});
+      queryClient.invalidateQueries({queryKey:["jobs"]});
+      queryClient.invalidateQueries({queryKey:["stats"]});
+      queryClient.invalidateQueries({queryKey:["charts"]});
+
+      router.push("/jobs");
+    }
+
+    
+  });
+
   function onSubmit(values:CreateAndEditJobType) {
     console.log(values);
+    mutate(values);
 
   }
 
@@ -62,7 +87,7 @@ function CreateJobForm() {
               type='submit'
               className='self-end capitalize'            
               >
-                 직업 생성
+                 {isPending ? "직업 생성중..." : "직업 생성"}
              </Button>
              
         </div>
