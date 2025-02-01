bf13bcadf463101a7234e9e35eef7b659a61bb7f
braverokmc79 <braverokmc79@gmail.com>
2025-02-01 18:18:16 +0900

    21. SearchForm Component - Functionality

--- src/components/JobsList.tsx
+++ src/components/JobsList.tsx
@@ -1,6 +1,17 @@
+import { getAllJobsAction } from '@/actions/jobService';
+import { useQuery } from '@tanstack/react-query';
+import { useSearchParams } from 'next/navigation';
 import React from 'react'
 
 const JobsList = () => {
+
+  const searchParams = useSearchParams();
+
+  const {data, isPending}=useQuery({
+    queryKey: ['jobs'],
+    queryFn: () => getAllJobsAction({}),
+  })
+
   return (
     <div>JobsList</div>
   )
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -4,9 +4,20 @@ import {Input} from './ui/input';
 import {Button} from './ui/button';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { JobStatus } from '@/types/JobDTO';
+import { useRouter ,usePathname, useSearchParams } from 'next/navigation';
 
 
 const SearchForm:React.FC = () => {
+
+  const searchParams = useSearchParams();
+
+  const search = searchParams.get('search') || '';
+  const jobStatus = searchParams.get('jobStatus') || '전체';
+  console.log("searchParams search :",search, "  :::: searchParams jobStatus:",jobStatus);
+
+
+  const router = useRouter();
+  const pathname = usePathname();
 
   const handleSubmit =(e:React.FormEvent<HTMLFormElement>)=>{
     e.preventDefault();
@@ -15,21 +26,28 @@ const SearchForm:React.FC = () => {
     const search=formData.get('search') as string;
     const jobStatus = formData.get('jobStatus') as string;
     console.log("search :",search, "  :::: jobStatus:",jobStatus);
+    
+    const params = new URLSearchParams();
+    params.set('search', search); 
+    params.set('jobStatus', jobStatus);
+
+    router.push(`${pathname}?${params.toString()}`);
   }
+
 
   return (
     <form className='bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3  gap-4 rounded-lg'
         onSubmit={handleSubmit}
     >
-        <Input type="search"  placeholder='직업 검색' name="search"  required />
-        <Select name="jobStatus"  defaultValue="all">
+        <Input type="search"  placeholder='직업 검색' name="search"  required defaultValue={search} />
+        <Select name="jobStatus"  defaultValue={jobStatus} >
             <SelectTrigger>
                 <SelectValue />
             </SelectTrigger>
             <SelectContent >
                 {
 
-                    ['all', ...Object.values(JobStatus)].map((jobStatus) => {
+                    ['전체', ...Object.values(JobStatus)].map((jobStatus) => {
                         return ( 
                         <SelectItem key={jobStatus} value={jobStatus}>
                             {jobStatus}
