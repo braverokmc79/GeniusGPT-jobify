71d9051de1fd251245776f027b1bc2202005d5a0
braverokmc79 <braverokmc79@gmail.com>
2025-02-01 17:49:40 +0900

    20.SearchForm Component - Setup

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,11 +1,64 @@
+"use client"
 import React from 'react'
+import {Input} from './ui/input';
+import {Button} from './ui/button';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { JobStatus } from '@/types/JobDTO';
 
-const SearchForm = () => {
+
+const SearchForm:React.FC = () => {
+
+  const handleSubmit =(e:React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const search=formData.get('search') as string;
+    const jobStatus = formData.get('jobStatus') as string;
+    console.log("search :",search, "  :::: jobStatus:",jobStatus);
+  }
+
   return (
-    <div>SearchForm</div>
+    <form className='bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3  gap-4 rounded-lg'
+        onSubmit={handleSubmit}
+    >
+        <Input type="search"  placeholder='직업 검색' name="search"  required />
+        <Select name="jobStatus"  defaultValue="all">
+            <SelectTrigger>
+                <SelectValue />
+            </SelectTrigger>
+            <SelectContent >
+                {
+
+                    ['all', ...Object.values(JobStatus)].map((jobStatus) => {
+                        return ( 
+                        <SelectItem key={jobStatus} value={jobStatus}>
+                            {jobStatus}
+                        </SelectItem>);                    
+                    })
+                    
+                }
+            </SelectContent>
+        </Select>
+        <Button type='submit'>검색</Button>
+    </form>
   
 )
 };
 
 
 export default SearchForm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
